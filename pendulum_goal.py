89cf9cb133f0c3d11613a29af6e73a00eaf1ab05
Dale Lukas Peterson <hazelnusse@gmail.com>
2010-08-11 16:33:24 -0700

    Made the code simpler.

--- pendulum_goal.py
+++ pendulum_goal.py
@@ -5,7 +5,7 @@ def pendulum_torque(t):
     return 0 #10*sin(2*pi*t + pi/6)
 
 def pendulum_f(x, t, params):
-    g, l, m, z = params[0]
+    g, l, m, z = params
     theta = x[0]
     thetap = x[1]
 
@@ -14,7 +14,7 @@ def pendulum_f(x, t, params):
     f[0] = thetap
     f[1] = -g/l*z + pendulum_torque(t) / (m*l*l)
     #f[1] = -g/l*sin(theta) + pendulum_torque(t) / (m*l*l)
-    params[0][3] = z
+    params[3] = z
     return f
 
 
