a370f5da762e4205c92c3a3820e5080593a5a144
Jason Moore <moorepants@gmail.com>
2010-08-13 12:51:56 -0700

    changed the input file up a bit, just some ideas

--- pendulum.py
+++ pendulum.py
@@ -1,6 +1,12 @@
-# State ordering
-# x[0] = theta
-# x[1] = theatap
+# States
+# The state order and the default initial conditions
+omega = 0.0
+theta = 0.0
+# This gets mapped to an ordered dictionary of the states, description and
+# intial conditions
+# x = {}
+# x['0'] = ['omega', 0.0]
+# x['1'] = ['theta', 0.0]
 
 # Parameters (default numerical values)
 m = 1.0
@@ -8,8 +14,11 @@ g = 9.81
 l = 1.0
 
 # Equations of Motion
-f[0] = thetap
-f[1] = -g/l*sin(theta)
+omegap = -g/l*sin(theta)
+thetap = omega
+# this gets mapped to the following in the integration function
+# f[0] = -g/l*sin(theta)
+# f[1] = omega
 
 # Model outputs
 ke = 1/2*m*(l*thetap)**2
