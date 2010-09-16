3f6d089648bdd57135ffc4e1c51704a1049927f2
Jason Moore <moorepants@gmail.com>
2010-09-16 10:04:41 -0700

    I have a basic working dynamicsystem class for a pendulum

---
+++ test_DyanmicSystem.py
@@ -0,0 +1,22 @@
+import DynamicSystem as ds
+
+pen = ds.DynamicSystem()
+
+print "Name of the system: ", pen.name
+
+print "Parameters: ", pen.parameters
+
+print "List of state names: ", pen.states
+
+print "Initial conditions: ", pen.x
+
+print "Integration parameters: ", pen.numint
+
+print "Evalute f: ", pen.f([1., 2.], 5.0)
+
+print "Evalute the input: ", pen.inputs(5.)
+
+print "Simulate and plot"
+pen.simulate()
+
+pen.plot()
