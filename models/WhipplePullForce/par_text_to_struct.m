63afcd9beb63f3cf1e3259a26b2c072866cd42ca
Jason Moore <moorepants@gmail.com>
2011-04-01 14:52:45 -0700

    par_text_to_struct seems to work

---
+++ models/WhipplePullForce/par_text_to_struct.m
@@ -0,0 +1,21 @@
+function par = par_text_to_struct(pathToFile)
+% Returns a structure with the the parameters in a text file.
+%
+% Parameters
+% ----------
+% pathToFile : string
+%   Path to a text file containing the benchmark parameters for a single
+%   bicycle.
+% Returns
+% -------
+% par : structure
+%   A structure containing the bicycle parameters.
+
+fid = fopen(pathToFile)
+data = textscan(fid, '%s %f', 'delimiter', ',')
+fclose(fid)
+names = data{1}
+vals = data{2}
+for i = 1:length(names)
+    par.(names{i}) = vals(i);
+end
