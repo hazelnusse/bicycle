347b322ca40997e49de10b87d6221a0e7d1eb325
Jason Moore <moorepants@gmail.com>
2011-04-14 21:30:51 -0700

    moved the bicycle parameter stuff to a new repo

--- BicycleParameters/BicycleParametersPlayground.py
+++
@@ -1,8 +0,0 @@
-import BicycleParameters as BP
-
-# see what happens when you don't have a directory with data
-booger = BP.Bicycle('booger')
-
-Browser = BP.Bicycle('Browser')
-
-Browser.calculate_from_measured()
